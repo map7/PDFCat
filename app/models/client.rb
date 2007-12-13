6a9028638f7cdd5c41b47aa0929d1edbfe8d0bda
(no author) <(no author)@d73ca4c0-4f8e-dc11-baea-00112f300e52>
2007-12-13 00:49:57 +0000

    allowed nil values for email
    git-svn-id: http://paistram/svn/repository/pdfcat/trunk@668 d73ca4c0-4f8e-dc11-baea-00112f300e52

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,5 +2,5 @@ class Client < ActiveRecord::Base
 	has_many :pdfs
 	validates_presence_of :name
 	validates_uniqueness_of :name
-	validates_format_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i
+	validates_format_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$|.*/i 
 end
