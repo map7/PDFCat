91b907d6a3413ac5774d83fb127a31cd8e9ff817
(no author) <(no author)@d73ca4c0-4f8e-dc11-baea-00112f300e52>
2008-01-15 01:05:53 +0000

    fixed to work on production machine
    git-svn-id: http://paistram/svn/repository/pdfcat/trunk@693 d73ca4c0-4f8e-dc11-baea-00112f300e52

--- fix_permissions.rb
+++ fix_permissions.rb
@@ -10,17 +10,22 @@ require 'fileutils'
 STORE_DIR = "/usr/tram/work/clt"
 
 # Connect to postgres database
-db = PGconn.connect('localhost', 5432, '', '', 'pdfcat_development', 'pdfcat', 'pdfcat')
+db = PGconn.connect('localhost', 5432, '', '', 'pdfcat_production', 'pdfcat', 'pdfcat')
 
 # Query the database
-res = db.exec('select c.name,cat.name,filename from clients as c,categories as cat,pdfs')
+res = db.exec('select c.name,cat.name,filename from clients as c,categories as cat,pdfs as p where p.client_id = c.id and p.category_id = cat.id ')
 
 # Display every path to every file in the database
 res.each do |row|
 	@filepath=STORE_DIR + '/' +row[0].downcase+'/'+row[1].downcase+'/'+row[2]
 	print 'file path = ',@filepath
 
-	FileUtils.chmod 0664, @filepath if File.exist?(@filepath)
+	if File.exist?(@filepath)
+		FileUtils.chmod 0664, @filepath 
+		FileUtils.chown 'scanner','www', @filepath
+	else
+		print "FILE DOES NOT EXIST!!!"
+	end
 	
 #	row.each do |column|
 #		print column
