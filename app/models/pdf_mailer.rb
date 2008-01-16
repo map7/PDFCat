a5d39dd11a5242d934503f48ee12a87acd555bc9
(no author) <(no author)@d73ca4c0-4f8e-dc11-baea-00112f300e52>
2008-01-16 22:04:02 +0000

    fixed sender address
    git-svn-id: http://paistram/svn/repository/pdfcat/trunk@713 d73ca4c0-4f8e-dc11-baea-00112f300e52

--- app/models/pdf_mailer.rb
+++ app/models/pdf_mailer.rb
@@ -3,7 +3,7 @@ class PdfMailer < ActionMailer::Base
   def email_client(recipient,subject,body,pdf)
     @recipients = recipient
     @subject    = pdf.client.name.upcase + ' - ' + pdf.pdfname + ' ' + pdf.pdfdate.to_s
-	@from       = 'mary@tramonana.com.au'
+	@from       = 'mary@tramontana.com.au'
     @sent_on    = Time.now 
 	@content_type='text/html'
     @body       = {:pdf => pdf, :body => body}
