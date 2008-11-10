3c15147dbee25793875432fa3a5f10932007c6d6
(no author) <(no author)@d73ca4c0-4f8e-dc11-baea-00112f300e52>
2008-11-10 03:40:15 +0000

    Add custom form builder 'SuperFormBuilder' Configure SuperFormBuilder as the default for all forms Start simplifying code in forms
    git-svn-id: http://paistram/svn/repository/pdfcat/trunk@1291 d73ca4c0-4f8e-dc11-baea-00112f300e52

---
+++ config/initializers/application.rb
@@ -0,0 +1,3 @@
+# Load my custom formbuilder as the default for all forms.
+
+ActionView::Base.default_form_builder = SuperFormBuilder
---
+++ lib/super_form_builder.rb
@@ -0,0 +1,46 @@
+#
+# Custom FormBuilder
+#
+# Author:  Michael Pope
+# Version: 1.00
+
+class SuperFormBuilder < ActionView::Helpers::FormBuilder
+
+  # Create an array of helpers to override with our label builder
+  helpers = field_helpers +
+            %w{calendar_date_select date_select datetime_select time_select} +
+            %w{collection_select select country_select time_zone_select} -
+            %w{hidden_field label fields_for} # Don't decorate these
+
+  helpers.each do |name|
+
+    # name = the type of object (text_field, etc)
+    define_method(name) do |field, *args|
+
+      # Get the hash option (EG the field involved)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+
+      # Create a label for that field
+      label = label(field, options[:label], :class => options[:label_class])
+
+      # Create an inline error
+      error = error_message_on(field)
+
+      # Wrap label in paragraph, include the original helper EG: text-field
+      @template.content_tag(:p, label +'<br/>' + super + error)  #wrap with a paragraph
+
+    end
+  end
+
+  # Override the submit button
+  def submit
+    # Calculate custom text for submit button.
+    prefix = object.new_record? ? "Create" : "Update"
+
+    # Wrap submit button in paragraph tags and add custom text.
+    @template.content_tag(:p,super(prefix, :id => "apply" ))
+  end
+
+
+end
+
