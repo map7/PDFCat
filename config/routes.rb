9cba35c7ae6e1597da35db305a0e3db080e34cd5
(no author) <(no author)@d73ca4c0-4f8e-dc11-baea-00112f300e52>
2008-11-10 03:44:07 +0000

    Start simplifying code in forms for category
    git-svn-id: http://paistram/svn/repository/pdfcat/trunk@1292 d73ca4c0-4f8e-dc11-baea-00112f300e52

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resources :clients
+  map.resources :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
 
