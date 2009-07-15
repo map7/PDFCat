265450a840a0bcaebf5fa3e2e1999e3f945da778
Michael Pope <michael@dtcorp.com.au>
2009-07-15 15:45:00 +1000

    Link users to firms through the new users form. Allowed delete function for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,23 +10,38 @@ class UsersController < ApplicationController
   # render new.rhtml
   def new
     @user = User.new
+    @firms = Firm.find(:all)
   end
 
   def create
     logout_keeping_session!
     @user = User.new(params[:user])
+
+    firm_id = params[:firm][:firm_id]
+    @user.firm_id = firm_id
+
     success = @user && @user.save
+
     if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
+      # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
+
       self.current_user = @user # !! now logged in
-      redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      redirect_to(users_url)
+      flash[:notice] = "User #{@user.login} added."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      @firms = Firm.find(:all)
       render :action => 'new'
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to(users_url)
   end
 end
