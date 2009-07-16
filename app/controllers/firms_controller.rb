e1f005e8e3a46629adfd4c34b378133381264b2d
Michael Pope <michael@dtcorp.com.au>
2009-07-16 10:32:34 +1000

    Changed version number Cleaned up firms controller Add validation for directories when creating a firm.

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -22,44 +22,29 @@ class FirmsController < ApplicationController
   def create
     @firm = Firm.new(params[:firm])
 
-    respond_to do |format|
-      if @firm.save
-        flash[:notice] = 'Firm was successfully created.'
-        format.html { redirect_to(@firm) }
-        format.xml  { render :xml => @firm, :status => :created, :location => @firm }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @firm.errors, :status => :unprocessable_entity }
-      end
+    if @firm.save
+      flash[:notice] = 'Firm was successfully created.'
+      redirect_to(firms_url)
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /firms/1
-  # PUT /firms/1.xml
   def update
     @firm = Firm.find(params[:id])
 
-    respond_to do |format|
-      if @firm.update_attributes(params[:firm])
-        flash[:notice] = 'Firm was successfully updated.'
-        format.html { redirect_to(@firm) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @firm.errors, :status => :unprocessable_entity }
-      end
+    if @firm.update_attributes(params[:firm])
+      flash[:notice] = 'Firm was successfully updated.'
+      redirect_to(firms_url)
+    else
+      render :action => "new"
     end
   end
 
-  # DELETE /firms/1
-  # DELETE /firms/1.xml
   def destroy
     @firm = Firm.find(params[:id])
     @firm.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(firms_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(firms_url)
   end
 end
--- app/models/firm.rb
+++ app/models/firm.rb
@@ -2,8 +2,30 @@ class Firm < ActiveRecord::Base
 
   has_many :users
 
+  validates_presence_of :name
+  validate :upload_dir_does_not_exist
+  validate :store_dir_does_not_exist
+
+  # Validate that the directory exists.
+  def upload_dir_exists?
+    File.exists?(upload_dir)
+  end
+
+  def upload_dir_does_not_exist
+    errors.add("upload_dir", "does not exist") unless upload_dir_exists?
+  end
+
+  def store_dir_exists?
+    File.exists?(store_dir)
+  end
+
+  def store_dir_does_not_exist
+    errors.add("store_dir", "does not exist") unless store_dir_exists?
+  end
 
 
+
+  # Permissions
   def is_updatable_by(user)
     user.is_admin?
   end
