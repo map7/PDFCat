fad1338297d2446f4feee2c2270eb62f4e1e0783
Michael Pope <michael@dtcorp.com.au>
2009-08-03 11:50:01 +1000

    Add default controller for pdfcat

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up ''
   # -- just remember to delete public/index.html.
-
+  map.root :controller => "pdfs"
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
