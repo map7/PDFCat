fb02375fdfa7caabf40c29e408df1c8d04de5d66
Michael Pope <michael@dtcorp.com.au>
2009-08-28 15:13:44 +1000

    Optmised firm

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -3,48 +3,11 @@ class FirmsController < ApplicationController
   before_filter :login_required
   before_filter :has_permission?
 
-  def index
-    @firms = Firm.paginate(:page => params[:page], :per_page => 10, :order => 'upper(name)')
-  end
+  make_resourceful do
+    actions :all
 
-  def show
-    @firm = Firm.find(params[:id])
-  end
-
-  def new
-    @firm = Firm.new
-  end
-
-  def edit
-    @firm = Firm.find(params[:id])
-  end
-
-  def create
-    @firm = Firm.new(params[:firm])
-
-    if @firm.save
-      flash[:notice] = 'Firm was successfully created.'
-      redirect_to(firms_url)
-    else
-      render :action => "new"
+    before :index do
+      @firms = Firm.paginate(:page => params[:page], :per_page => 10, :order => 'upper(name)')
     end
-  end
-
-  def update
-    @firm = Firm.find(params[:id])
-
-    if @firm.update_attributes(params[:firm])
-      flash[:notice] = 'Firm was successfully updated.'
-      redirect_to(firms_url)
-    else
-      render :action => "new"
-    end
-  end
-
-  def destroy
-    @firm = Firm.find(params[:id])
-    @firm.destroy
-
-    redirect_to(firms_url)
   end
 end
