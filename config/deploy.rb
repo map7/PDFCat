b66c2f317d0bfb75823826273ebd9d89533df3fe
Michael Pope <michael@dtcorp.com.au>
2009-11-09 14:32:40 +1100

    Capify pdfcat

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+set :application, "pdfcat"
+set :repository,  "apps:/home/map7/pdfcat"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to, "/usr/local/www/pdfcat"
+
+role :web, "paistram.lan"                          # Your HTTP server, Apache/etc
+role :app, "paistram.lan"                          # This may be the same as your `Web` server
+role :db,  "paistram.lan", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start {}
+#   task :stop {}
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
