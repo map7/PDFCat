2e02de523d5e36594e449eccadba1ef73b102788
Michael Pope <michael@dtcorp.com.au>
2009-11-10 15:59:27 +1100

    Added namespace for delayed job to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,24 @@ deploy.task :restart, :roles => :app do
   run "touch #{current_path}/tmp/restart.txt"
 end
 
+# For delayed job
+namespace :delayed_job do
+  desc "Stop the delayed_job process"
+  task :stop, :roles => :app do
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job stop"
+  end
+
+  desc "Start the delayed_job process"
+  task :start, :roles => :app do
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job start"
+  end
+
+  desc "Restart the delayed_job process"
+  task :restart, :roles => :app do
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart"
+  end
+end
+
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
 after "deploy:restart", "delayed_job:restart"
