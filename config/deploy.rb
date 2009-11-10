b163d1242c0ebd6d69c2886abdf689393a133b8b
Michael Pope <michael@dtcorp.com.au>
2009-11-10 14:23:32 +1100

    Changed capistrano to work with Passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,10 +15,10 @@ role :db,  "paistram.lan", :primary => true # This is where Rails migrations wil
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start {}
-#   task :stop {}
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start {}
+  task :stop {}
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
