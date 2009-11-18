d80206b847fc7ef1ecd69c554df799fd4a048c42
Michael Pope <michael@dtcorp.com.au>
2009-11-19 09:59:25 +1100

    Version 2.1.24

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
-PDFCAT_VERSION="2.1.23"
+PDFCAT_VERSION="2.1.24"
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
