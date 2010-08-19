21f62885ab5cd158eacb98e9b4ac3571ebbbfb98
Michael Pope <map7777@gmail.com>
2010-08-19 15:20:47 +1000

    upgraded rails & put default user creation into seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# Build default data
+# rake db:seed
+
+password = 'pdfcat'
+
+u = User.new(:login => 'admin',
+             :password => password,
+             :password_confirmation => password,
+             :is_admin => true,
+             :firm_id => 1)
+
+u.crypted_password = u.encrypt(password)
+u.save
+
