0851cb8810f9ffa945e59fa3d0f756a752a44d71
Michael Pope <map7777@gmail.com>
2010-08-23 09:58:48 +1000

    added haml to environment and bumped up version

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
-PDFCAT_VERSION="2.1.25"
+PDFCAT_VERSION="2.2"
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -28,6 +28,8 @@ Rails::Initializer.run do |config|
     :port => "25",
     :domain => "lan"
   }
+  
+  config.gem "haml"
 
   # RESTful ACL support.
   config.gem "mdarby-restful_acl", :lib => 'restful_acl_controller'
