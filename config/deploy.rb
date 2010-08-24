a49bb59195d9f5fd3374875f9ec7a19a0a1485d3
Michael Pope <map7777@gmail.com>
2010-08-24 15:18:18 +1000

    Added depechemode, bumped version

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ namespace :submodules do
     # Initialise submodules
     run "cd #{current_path}; git submodule update -i public/javascripts/jquery.beeline/"
     run "cd #{current_path}; git submodule update -i public/javascripts/jquery.depechemode/"
+    run "cd #{current_path}; git submodule update -i public/javascripts/jquery.depechemode/jquery.livequery"
     run "#{try_sudo} chmod -R +x #{release_path}"
   end
 end
