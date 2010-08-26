e3a4d0c2de64085e2d3443524569723b31b47d28
Michael Pope <map7777@gmail.com>
2010-08-26 16:28:38 +1000

    Made sure show, edit and delete work off ajax for categories.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,6 +11,8 @@ function rebind(){
 	field_down: 40,
 	submit_key_code: 123});
 
+    $('.help_btn').click(function(e){ $('div#help').toggle(); });
+    
     $.focus_input();  // Set focus in form.
 };
 
@@ -18,11 +20,22 @@ $(document).beeline({field_keys: ['esc','f1','f2', 'f3', 'f4', 'f5', 'f6', 'f7',
 $(document).depechemode({container: "contentInner"} );
 
 $(document).ready(function(event){
+    rebind();
+    
     // Ajax navigation
     $('a.ajax').live('click', function(e) { 
 	e.preventDefault();
 	link = $(this); 
 	target = link.attr('target') === '' ? 'contentInner' : link.attr('target');
+
+	switch (link.attr('id')){
+	case "destroy": 
+	    $.post(link.attr('href'), '_method=delete', null, 'script');
+	    href = link.attr('return');
+	    break;
+	default:
+	    href = link.attr('href');
+	}
 
 	$('#'+target).load(link.attr('href'), function() {
 	    $('#'+target).ready(function(evt){ rebind(); });
