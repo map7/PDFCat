f1b6ed06abedd333e7a6ddd2cbd7baed2c24f5d4
Michael Pope <map7777@gmail.com>
2011-11-28 15:38:42 +1100

    Remove old validation no longer required

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,9 +9,6 @@ class Category < ActiveRecord::Base
   validates_uniqueness_of :name, :case_sensitive => false, :scope => :firm_id
   validates_format_of :name, :with => /^[(|)|A-Z|a-z|0-9][,|&|(|)|'| |.|\-|A-Z|a-z|0-9]+$/
 
-  # Just use the directory if it exists, don't deny people the right to use an existing dir.
-  # validate :new_dir_available?   
-
   default_scope :order => "upper(name)"
   
   def self.per_page
@@ -42,17 +39,5 @@ class Category < ActiveRecord::Base
         File.rename(@old_dir,pdf.full_dir)
       end
     end
-  end
-
-  # Check if new proposed directory exists under any client with the category.
-  def new_dir_available?
-    if self.name_changed?
-      self.clients.each do|client|
-        if File.exists?("#{client.firm.store_dir}/#{client.name}/#{self.name}".downcase)
-          errors.add(:name, "Category directory exists for some clients")
-        end
-      end
-    end
-    return errors.count == 0
   end
 end
