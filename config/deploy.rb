534ee1e3a7a3c57d3c2536a33e38cbc41a467c90
Michael Pope <map7777@gmail.com>
2011-12-06 13:21:48 +1100

    fix up deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,17 +40,17 @@ end
 namespace :delayed_job do
   desc "Stop the delayed_job process"
   task :stop, :roles => :app do
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} script/delayed_job stop"
+    run "cd #{release_path}; #{try_sudo} RAILS_ENV=#{rails_env} script/delayed_job stop"
   end
 
   desc "Start the delayed_job process"
   task :start, :roles => :app do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job start"
+    run "cd #{current_path}; #{try_sudo} RAILS_ENV=#{rails_env} script/delayed_job start"
   end
 
   desc "Restart the delayed_job process"
   task :restart, :roles => :app do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart"
+    run "cd #{current_path}; #{try_sudo} RAILS_ENV=#{rails_env} script/delayed_job restart"
   end
 end
 
