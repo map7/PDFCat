1c6e39e7df10ac399f88d96b3141948e81ca107b
Michael Pope <michael@dtcorp.com.au>
2013-03-04 12:09:20 +1100

    Remove the version as you can tell what is deployed through git

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 # RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
-PDFCAT_VERSION="2.3.2"
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require 'thread'
 require File.join(File.dirname(__FILE__), 'boot')
