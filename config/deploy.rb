027ff1d3b588231d2e90a84760cd08a859ec7a13
Michael Pope <michael@dtcorp.com.au>
2016-01-22 12:47:42 +1100

    FIX: restarting of delayed job after deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,22 +50,22 @@ end
 namespace :delayed_job do
   desc "Stop the delayed_job process"
   task :stop, :roles => :app do
-    run "cd #{release_path}; rbenv sudo RAILS_ENV=#{rails_env} script/delayed_job stop"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job stop"
   end
 
   desc "Start the delayed_job process"
   task :start, :roles => :app do
-    run "cd #{current_path}; rbenv sudo RAILS_ENV=#{rails_env} script/delayed_job start"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job start"
   end
 
   desc "Restart the delayed_job process"
   task :restart, :roles => :app do
-    run "cd #{current_path}; rbenv sudo RAILS_ENV=#{rails_env} script/delayed_job restart"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart"
   end
 end
 
-# after "deploy:stop",    "delayed_job:stop"
-# after "deploy:start",   "delayed_job:start"
+before "deploy",    "delayed_job:stop"
+after "deploy",   "delayed_job:start"
 after "deploy:restart",   "submodules:init"
 # after "deploy:restart", "delayed_job:restart"
 after "deploy", "deploy:migrate"
