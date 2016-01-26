f21c9ddfdcab72b02243f3fcba5c2be59417f1ae
Michael Pope <michael@dtcorp.com.au>
2016-01-27 10:08:27 +1100

    Ignore if we have already got a thumbnail in place

--- app/models/pdf_thumbnail.rb
+++ app/models/pdf_thumbnail.rb
@@ -4,7 +4,7 @@ class PdfThumbnail
   # Create a thumbnail of the first pdf page
   def self.make_thumbnail(original_pdf)
     PdfThumbnail.create_dir(original_pdf)
-    if File.exists?(original_pdf.full_path)
+    if File.exists?(original_pdf.full_path) && !File.exists?(original_pdf.thumbnail_full_path)
       pdf=Magick::ImageList.new(original_pdf.full_path)
       thumb=pdf.scale(180,250)
       thumb.write(original_pdf.thumbnail_full_path)
