84f8346c4b3898b325fba47f902e19bb81f3b5da
Michael Pope <michael@dtcorp.com.au>
2016-02-15 13:52:04 +1100

    Separate file based tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -22,91 +22,98 @@ end
 
 describe Category do
 
-  describe "#move_dir" do 
+  context "with file operations" do 
     before do
       FakeFS.activate!
-      make_basedirs
-
-      @pdf = Pdf.make
-      @client_dir = "#{@pdf.firm.store_dir}/publishing solutions"
-      @cat = @pdf.category
     end
 
     after do
       FakeFS.deactivate!      
     end
 
-    context "when changing the main category" do
-      before do 
-        make_dir_with_file("#{@client_dir}/general", "Unit_Trust-20100128.pdf") # Make the old directory
-        @cat.name = "new_name" # Change the category name
-      end
-    
-      context "new dir doesn't exist" do 
-        it "will rename the old directory" do
-          @cat.move_dir
-          assert File.exists?("#{@client_dir}/new_name")
-        end
-      end
-
-      context "new dir does exist" do
-        it "will move each pdf" do
-          FileUtils.mkdir_p("#{@client_dir}/#{@cat.name}")   # Make the new dir
-          @cat.move_dir
-          assert File.exists?("#{@client_dir}/#{@cat.name}/Unit_Trust-20100128.pdf")
-        end
-      end
-
-      context "old directory has already been renamed" do
-        it "should not rename the file" do
-          FileUtils.mv("#{@client_dir}/general","#{@client_dir}/new_name") # Rename old to new move_dir.
-          @cat.move_dir
-          assert File.exists?("#{@client_dir}/#{@cat.name}/Unit_Trust-20100128.pdf")
-        end
-      end
-    end                         # main cat
-
-    context "when it's a sub-category" do
+    describe "#move_dir" do 
       before do
-        @sub = make_sub(@cat, "sub") # current dir
-        make_dir_with_file("#{@client_dir}/#{@cat.name.downcase}/#{@sub.name}","Unit_Trust-20100128.pdf")
-        @pdf.update_attribute(:category_id, @sub.id) # Make sure pdf is part of the sub category
-        @newsub = make_sub(@cat, "newsub")
+        make_basedirs
+
+        @pdf = Pdf.make
+        @client_dir = "#{@pdf.firm.store_dir}/publishing solutions"
+        @cat = @pdf.category
       end
 
-      context "new dir doesn't exist" do
-        it "exists before the move" do
-          assert File.exists?("#{@client_dir}/#{@cat.name.downcase}/#{@sub.name}/Unit_Trust-20100128.pdf")
+
+      context "when changing the main category" do
+        before do 
+          make_dir_with_file("#{@client_dir}/general", "Unit_Trust-20100128.pdf") # Make the old directory
+          @cat.name = "new_name" # Change the category name
         end
         
-        it "will renames the old dir" do
-          @sub.name = "newsub"
-          @sub.move_dir
-          assert File.exists?("#{@client_dir}/#{@cat.name.downcase}/#{@newsub.name}/Unit_Trust-20100128.pdf")
-        end        
-      end
-
-      context "new dir does exist" do
-        it "will move the pdf" do
-          FileUtils.mkdir_p "#{@client_dir}/#{@cat.name.downcase}/#{@newsub.name}"
-          @sub.name = "newsub"
-          @sub.move_dir
-          assert File.exists?("#{@client_dir}/#{@cat.name.downcase}/#{@newsub.name}/Unit_Trust-20100128.pdf")
+        context "new dir doesn't exist" do 
+          it "will rename the old directory" do
+            @cat.move_dir
+            assert File.exists?("#{@client_dir}/new_name")
+          end
         end
-      end
 
-      context "old dir has already been renamed" do
-        it "should not rename the file" do
-          FileUtils.mv("#{@client_dir}/#{@cat.name.downcase}/#{@sub.name}","#{@client_dir}/#{@cat.name.downcase}/other_dir")
-          @sub.name = "newsub"
-          @sub.move_dir
-          assert File.exists?("#{@client_dir}/#{@cat.name.downcase}/other_dir/Unit_Trust-20100128.pdf")
+        context "new dir does exist" do
+          it "will move each pdf" do
+            FileUtils.mkdir_p("#{@client_dir}/#{@cat.name}")   # Make the new dir
+            @cat.move_dir
+            assert File.exists?("#{@client_dir}/#{@cat.name}/Unit_Trust-20100128.pdf")
+          end
         end
-      end
-    end # sub
+
+        context "old directory has already been renamed" do
+          it "should not rename the file" do
+            FileUtils.mv("#{@client_dir}/general","#{@client_dir}/new_name") # Rename old to new move_dir.
+            @cat.move_dir
+            assert File.exists?("#{@client_dir}/#{@cat.name}/Unit_Trust-20100128.pdf")
+          end
+        end
+      end                         # main cat
+
+      context "when it's a sub-category" do
+        before do
+          @sub = make_sub(@cat, "sub") # current dir
+          make_dir_with_file("#{@client_dir}/#{@cat.name.downcase}/#{@sub.name}","Unit_Trust-20100128.pdf")
+          @pdf.update_attribute(:category_id, @sub.id) # Make sure pdf is part of the sub category
+          @newsub = make_sub(@cat, "newsub")
+        end
+
+        context "new dir doesn't exist" do
+          it "exists before the move" do
+            assert File.exists?("#{@client_dir}/#{@cat.name.downcase}/#{@sub.name}/Unit_Trust-20100128.pdf")
+          end
+          
+          it "will renames the old dir" do
+            @sub.name = "newsub"
+            @sub.move_dir
+            assert File.exists?("#{@client_dir}/#{@cat.name.downcase}/#{@newsub.name}/Unit_Trust-20100128.pdf")
+          end        
+        end
+
+        context "new dir does exist" do
+          it "will move the pdf" do
+            FileUtils.mkdir_p "#{@client_dir}/#{@cat.name.downcase}/#{@newsub.name}"
+            @sub.name = "newsub"
+            @sub.move_dir
+            assert File.exists?("#{@client_dir}/#{@cat.name.downcase}/#{@newsub.name}/Unit_Trust-20100128.pdf")
+          end
+        end
+
+        context "old dir has already been renamed" do
+          it "should not rename the file" do
+            FileUtils.mv("#{@client_dir}/#{@cat.name.downcase}/#{@sub.name}","#{@client_dir}/#{@cat.name.downcase}/other_dir")
+            @sub.name = "newsub"
+            @sub.move_dir
+            assert File.exists?("#{@client_dir}/#{@cat.name.downcase}/other_dir/Unit_Trust-20100128.pdf")
+          end
+        end
+      end # sub
+    end
   end
+
   
-  context "without fakefs" do 
+  context "without file operations" do 
     let(:cat) {Category.make}
     
     before do 
