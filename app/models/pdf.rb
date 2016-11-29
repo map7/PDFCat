ecf46e30c5a1c909349d42f602bcbf3bc9b4a5f4
Michael Pope <michael@dtcorp.com.au>
2016-11-29 16:57:20 +1100

    Revert back

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -441,7 +441,7 @@ class Pdf < ActiveRecord::Base
       logger.info "#{full_path} already ocr'd"
     else
       logger.info "OCR #{full_path}"
-      cmd = "abbyyocr --multiProcessingMode Parallel --recognitionProcessesCount 32 --progressInformation --useNotOnlyPhysicalCPUCores -if '#{full_path}' -f PDF -of '#{full_path}'
+      cmd = "abbyyocr --multiProcessingMode Parallel --recognitionProcessesCount 32 --progressInformation --useNotOnlyPhysicalCPUCores -if \"#{full_path}\" -f PDF -of \"#{full_path}\""
       logger.info "#{cmd}"
       status = system("#{cmd}")
       self.update_attribute(:ocr, true) if status
