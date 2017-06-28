e3d41471c03d8f1fc7d84bc46b8491ad3f364d84
Michael Pope <michael@dtcorp.com.au>
2017-06-28 11:36:01 +1000

    Thumbnail-Use poleica for thumbnails

--- app/models/pdf_thumbnail.rb
+++ app/models/pdf_thumbnail.rb
@@ -1,5 +1,6 @@
 class PdfThumbnail
-  require 'rmagick'
+  require 'fileutils'
+  THUMBNAIL_DIR="#{Rails.root}/public/images/thumbnails"
   
   # Create a thumbnail of the first pdf page
   def self.make_thumbnail(original_pdf)
@@ -10,18 +11,15 @@ class PdfThumbnail
   end
 
   def self.update_thumbnail(original_pdf)
-    begin
-      pdf=Magick::ImageList.new(original_pdf.full_path)
-      thumb=pdf.scale(180,250)
-      thumb.write(original_pdf.thumbnail_full_path)
-    rescue
-      puts "** Issues with: #{original_pdf.full_path}"
+    thumb = Poleica.new(original_pdf.full_path).to_png(width: 180, height: 250)
+
+    if File.exists?(thumb) & !File.exists("#{THUMBNAIL_DIR}/#{original_pdf.id}.png")
+      FileUtils.mv(thumb, "#{THUMBNAIL_DIR}/#{original_pdf.id}.png")      
     end
   end
   
   # Create the thumbnails directory
   def self.create_dir(pdf)
-    thumbnail_dir = "#{Rails.root}/public/images/thumbnails"
-    FileUtils.mkdir_p(thumbnail_dir) unless File.exists?(thumbnail_dir)
+    FileUtils.mkdir_p(THUMBNAIL_DIR) unless File.exists?(THUMBNAIL_DIR)
   end
 end
