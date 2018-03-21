4ecde225dfeee0cbca508e4294e47aab611f2c46
Michael Pope <michael@dtcorp.com.au>
2018-03-21 14:00:55 +1100

    PDF-Apply validations on description instead of pdfname

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -7,9 +7,9 @@ class Pdf < ActiveRecord::Base
   belongs_to :category
   belongs_to :client
 
-  validates_presence_of :pdfdate, :pdfname, :filename, :category_id, :client_id, :description
+  validates_presence_of :pdfdate, :filename, :category_id, :client_id, :description
   validates_uniqueness_of :pdfname, :scope => [:pdfdate, :category_id, :client_id, :firm_id]
-  validates_format_of :pdfname, :with => /^[^\/\\\?\*:|"<>]+$/, :message => "cannot contain any of the following characters: / \\ ? * : | \" < >"
+  validates_format_of :description, :with => /^[^\/\\\?\*:|"<>]+$/, :message => "cannot contain any of the following characters: / \\ ? * : | \" < >"
 
   named_scope :joined, :joins => [:firm, :client, :category]
   default_scope :order => 'pdfdate DESC'
